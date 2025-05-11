f05d0b5c685edb7978098b16a7ae28b88cb2802e
Ashwini R <ashwiniravi292006@gmail.com>
2025-05-11 21:21:00 +0530

    Day26

---
+++ remove duplicate from sorted array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n=nums.size();
+        int u=1;
+        
+        for(int i=1;i<n;i++){
+            if(nums[i]!=nums[i-1]){
+                
+                nums[u]=nums[i];
+                u++;
+            }
+            
+        }
+        return u;
+        
+    }
+};
